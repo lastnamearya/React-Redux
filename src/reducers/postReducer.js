647a83b7c41b772f9001c2950c47e2764cef996e
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-22 18:15:18 +0530

    Making a POST HTTP Request and rendering that newly added item into UI

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,6 +1,6 @@
 // Importing action types
 
-import { FETCH_POSTS, NEW_POSTS } from "../actions/types";
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
 
 const initialState = {
   items: [],
@@ -15,6 +15,11 @@ export default function(state = initialState, action) {
       return {
         ...state,
         items: action.payload
+      };
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload
       };
     default:
       return state;
